091c8025be3fbfb9f8518d0bc965081fa7ed1573
Mohammed Varaliya <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-08-03 18:30:41 +0530

    commit

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c3u!817y1z52g1e5phjau^s5399#6g^l^^0u%1^aoat_w0u95@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'iknowledgesystem.herokuapp.com']
 
