e5183ef1aa97795740cce63c857c32b6235f2f6c
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-10-15 12:11:19 +0530

    format posts

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'iCoder.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
