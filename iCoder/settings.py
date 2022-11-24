bea7a160ee12d6073159db3821eac6f21483ee92
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-24 11:57:03 +0530

    comm

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'iCoder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "iknowledge",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": '127.0.0.1',
-        "PORT": '3306',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'iknowledge',
+      'USER':'postgres',
+      'PASSWORD':'postgres',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
